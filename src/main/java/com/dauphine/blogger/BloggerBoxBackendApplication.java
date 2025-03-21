3ceb9b3ce80768b226ef6ad386bd524eb4e031ec
NicolasLONGHI <nico784474@gmail.com>
2025-03-21 14:52:43 +0100

    maj1

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -1,9 +1,21 @@
 package com.dauphine.blogger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
 
 @SpringBootApplication
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogger box backend",
+				description = "Blogger box endpoints and api",
+				contact = @Contact(name = "Nicolas", email = "nicolas.longhi@gmail.com"),
+				version = "1.0.0"
+		)
+)
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
